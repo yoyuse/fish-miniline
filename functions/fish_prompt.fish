b226f9882ced5bf9382e16efbcd073d2cd79c9d2
yoyuse <yoyuse@gmail.com>
2023-06-24 12:54:01 +0900

    feat: `$__miniline_use_powerline`

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,34 +1,78 @@
-function fish_prompt
-    set -l __status $status
+function __miniline_has_powerline
+    switch $__miniline_use_powerline
+        case 1 yes true
+            true
+        case 0 no false
+            false
+        case '*'
+            switch $TERM_PROGRAM
+                case iTerm.app WezTerm
+                    true
+                case '*'
+                    false
+            end
+    end
+end
 
-    set -l __arrow '' # \uE0B0
-    set -l __arrow2 '│' # \uE0B1
-    set -l __color blue
-    set -l __color2 $__color
-    set -l root root
+function __miniline_color
+    set_color normal
+    if __miniline_has_powerline
+        test -n "$argv[1]" && set_color $argv[1]
+        test -n "$argv[2]" && set_color -b $argv[2]
+    else
+        test -n "$argv[1]" && set_color -b $argv[1]
+        test -n "$argv[2]" && set_color $argv[2]
+    end
+end
+
+function __miniline_color_rev
+    set_color normal
+    test -n "$argv[1]" && set_color -b $argv[1]
+    test -n "$argv[2]" && set_color $argv[2]
+end
+
+function __miniline_l
+    __miniline_has_powerline &&
+    echo -n \uE0B6 ||           # 
+    echo -n ' '
+end
+
+function __miniline_r
+    __miniline_has_powerline &&
+    echo -n \uE0B4 ||           # 
+    echo -n ' '
+end
+
+function __miniline_c
+    echo -n ' | '
+end
+
+function fish_prompt
+    set -l st $status
+
+    set -l color blue
+    test $st -ne 0 && set color red
+    set -l color2 $color
     set -l __fg black
 
-    test $__status -eq 0 || set __color red
+    set prompt (__miniline_color_rev $color $__fg)(prompt_pwd)
 
-    test "$USER" = "$root" && set __color2 yellow
-
-    if test $SSH_TTY
-        echo -n (set_color -b magenta)(set_color $__fg)
-        echo -n ' '$USER'@'(prompt_hostname)' '
-        echo -n (set_color -b $__color2)(set_color magenta)$__arrow
+    if test $st -ne 0
+        set prompt (__miniline_color_rev red $__fg)$st(__miniline_c)"$prompt"
+        set color2 red
     end
 
-    if test "$USER" = "$root"
-        echo -n (set_color -b yellow)(set_color $__fg)
-        echo -n ' # '
-        echo -n (set_color -b $__color)(set_color yellow)$__arrow
+    if test "$USER" = "root"
+        set prompt (__miniline_color_rev yellow $__fg)'# '(__miniline_color_rev $color2 $__fg)" $prompt"
+        set color2 yellow
     end
 
-    echo -n (set_color -b $__color)(set_color $__fg)
+    if test -n "$SSH_TTY"
+        set prompt (__miniline_color_rev magenta $__fg)"$USER@"(prompt_hostname)' '(__miniline_color_rev $color2 $__fg)" $prompt"
+        set color2 magenta
+    end
 
-    test $__status -eq 0 || echo -n " $__status $__arrow2"
-
-    echo -n ' '(prompt_pwd)' '
-    echo -n (set_color normal)(set_color $__color)$__arrow
+    set prompt (__miniline_color $color2)(__miniline_l)$prompt(__miniline_color $color)(__miniline_r)
+    echo -n $prompt
     echo -n (set_color normal)' '
 end
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,13 +1,11 @@
 function fish_right_prompt
-    set -l __arrow '' # \uE0B2
-    set -l __arrow2 '│' # \uE0B3
-    set -l __fg black
+    set -l fg black
 
     set -l branch (git symbolic-ref HEAD 2>/dev/null | cut -d/ -f3)
     set -l git_stat (git status --porcelain 2>/dev/null | string collect)
-    set -l untracked (echo "$git_stat" | grep "??")
-    set -l index_stat (echo "$git_stat" | grep -v "?" | cut -d" " -f1)
-    set -l wtree_stat (echo "$git_stat" | grep -v "?" | cut -d" " -f2)
+    set -l untracked (echo "$git_stat" | grep '??')
+    set -l index_stat (echo "$git_stat" | grep -v '?' | cut -d' ' -f1)
+    set -l wtree_stat (echo "$git_stat" | grep -v '?' | cut -d' ' -f2)
     set -l index_stat (string join '' $index_stat)
     set -l wtree_stat (string join '' $wtree_stat)
     set -l upstream_stat (git status -sb 2>/dev/null | head -1)
@@ -15,22 +13,27 @@ function fish_right_prompt
     set -l behind (string match -r 'behind (\d+)\]' $upstream_stat | tail -1)
     set -l stash_stat (git stash list 2>/dev/null)
 
-    set -l _git ""
-    set -l flag ""
-    set -l __color
+    set -l git ''
+    set -l flag ''
+    set -l color
     if test -n "$branch"
-        set __color blue
+        set color blue
         test -n "$stash_stat" && set flag $flag'$'
-        test -n "$index_stat" && set __color green && set flag $flag'~' # i
-        test -n "$wtree_stat" && set __color red && set flag $flag'*' # w
-        test -n "$untracked" && set __color magenta && set flag $flag'?' # u
+        test -n "$index_stat" && set color green && set flag $flag'~' # i
+        test -n "$wtree_stat" && set color red && set flag $flag'*' # w
+        test -n "$untracked" && set color magenta && set flag $flag'?' # u
 
         test -n "$ahead" && set flag $flag'↑'$ahead
         test -n "$behind" && set flag $flag'↓'$behind
 
-        set _git " "$branch" "(set_color normal)
-        test $flag && set _git (set_color $__fg)" "$flag" "$__arrow2$_git
-        set _git (set_color $__color)$__arrow(set_color $__fg)(set_color -b $__color)$_git
+        set git (__miniline_color_rev $color $fg)"$branch"
+        if test -n "$flag"
+            set git "$flag"(__miniline_color_rev $color $fg)(__miniline_c)$git
+        end
     end
-    echo -n $_git
+    if test -n "$git"
+        set git (__miniline_color $color)(__miniline_l)(__miniline_color_rev $color $fg)$git(__miniline_color $color)(__miniline_r)
+        echo -n $git
+        echo -n (set_color normal)
+    end
 end
